aa17e3e9f341955a1ba8c75568005b49a3b939cf
Josh <joshuaeduque@gmail.com>
2024-10-26 15:00:51 -0500

    Added actions

---
+++ src/routes/+page.server.js
@@ -0,0 +1,8 @@
+export const actions = {
+    submitPrompt: async (event) => {
+        console.log('submitPrompt action executed server side');
+    },
+    regenerateList: async (event) => {
+        console.log('regenerateList action executed server side');
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,47 @@
 		<img class="w-28" src="/favicon.png" alt="FutureFocus logo" />
 		<p class="text-6xl">FutureFocus</p>
 	</div>
-	<div class="grid justify-center justify-items-center">
-		<form class="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md" action="todo" method="post">
-			<label for="">
-				<input placeholder="i just lost my dawg" type="text" />
-			</label>
-			<button
-				class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-				>Submit</button
+	<div class="grid justify-center justify-items-center gap-4">
+		<div>
+			<form
+				class="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md"
+				action="?/submitPrompt"
+				method="post"
 			>
-		</form>
+				<label for="">
+					<input
+						class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+						placeholder="i just lost my dawg"
+						type="text"
+					/>
+				</label>
+				<button
+					class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+					>Submit</button
+				>
+			</form>
+		</div>
+		<div class="grid">
+			<div>
+				<input type="checkbox" name="" id="" />
+				<label for="">Small</label>
+			</div>
+			<div>
+				<input type="checkbox" name="" id="" />
+				<label for="">Medium</label>
+			</div>
+			<div>
+				<input type="checkbox" name="" id="" />
+				<label for="">Large</label>
+			</div>
+		</div>
+		<div>
+			<form action="?/regenerateList" method="post">
+				<button
+					class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+					>Generate</button
+				>
+			</form>
+		</div>
 	</div>
 </div>
