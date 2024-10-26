2c0cb65c6f12a71f47fdec536c582b6e7c841916
Josh <joshuaeduque@gmail.com>
2024-10-26 14:32:09 -0500

    Added input form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+	<div class="flex flex-row items-center">
+		<img class="w-28" src="/favicon.png" alt="FutureFocus logo" />
+		<p class="text-6xl">FutureFocus</p>
+	</div>
+	<div class="grid justify-center justify-items-center">
+		<form class="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md" action="todo" method="post">
+			<label for="">
+				<input placeholder="i just lost my dawg" type="text" />
+			</label>
+			<button
+				class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+				>Submit</button
+			>
+		</form>
+	</div>
+</div>
