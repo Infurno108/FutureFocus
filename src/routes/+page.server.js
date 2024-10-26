6b8d622a08ffc9e07d9aa298efe2c88adc6fcdf6
Josh <joshuaeduque@gmail.com>
2024-10-26 16:12:26 -0500

    Added params

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,10 @@
 export const actions = {
-    submitPrompt: async (event) => {
-        console.log('submitPrompt action executed server side');
+    submitPrompt: async ({request}) => {
+        const data = await request.formData();
+        const promptInput = data.get('promptInput');
+        const checked = data.get('checked');
+
+        console.log(promptInput);
+        console.log(checked);
     }
 }
