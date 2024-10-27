917183e6d83d9cd57492c178b0a86bf2b8a10e0f
decloon <decstaun06@outlook.com>
2024-10-26 22:13:50 -0500

    terraform 6

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
     most_recent = true
 
     filter {
-        name   = "FutureFocus"
+        name   = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
     }
 
